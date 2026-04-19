f6855da914df27b1ee0da5c7b5e8696590c71239
Slidou <akrm.mnaa@gmail.com>
2026-04-19 23:54:31 +0200

    +1

--- journal-data.js
+++ journal-data.js
@@ -2,4 +2,5 @@
 const journalData = [
 { d: "2026-04-19", t: "livre", title: "Le Bouffon", img: "covers/lebouffon.jpg", note: "lu en entier" },
 { d: "2026-04-19", t: "film", title: "Sonatine - Mélodie mortelle", img: "films/sonatine.webp", note: "" },
+{ d: "2026-04-19", t: "anime", title: "Choro Mesu Days", img: "films/choromesudays.jpg", note: "1 épisode sur 2 pour le moment" },
 ];
