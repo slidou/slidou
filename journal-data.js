f33c36ff6bc1bd3d5052e66028555a129f27c360
Slidou <akrm.mnaa@gmail.com>
2026-04-21 03:02:41 +0200

    .

--- journal-data.js
+++ journal-data.js
@@ -2,7 +2,7 @@
 const journalData = [
 { d: "2026-04-19", t: "livre", title: "Le Bouffon", img: "covers/lebouffon.jpg", note: "lu en entier" },
 { d: "2026-04-19", t: "film", title: "Sonatine - Mélodie mortelle", img: "films/sonatine.webp", note: "" },
-{ d: "2026-04-19", t: "anime", title: "Choro Mesu Days", img: "anime/choromesudays.jpg", note: "1 épisode sur 2 pour l'instant", blur: true },
+{ d: "2026-04-19", t: "anime", title: "Choro Mesu Days", img: "anime/choromesudays.jpg", note: "épisode 1", blur: true },
 { d: "2026-04-20", t: "anime", title: "Nippon Sangoku", img: "anime/nipponsangoku.jpg", note: "épisode 3" },
 { d: "2026-04-20", t: "anime", title: "Marriagetoxin", img: "anime/marriagetoxin.jpg", note: "épisode 1" },
 { d: "2026-04-21", t: "anime", title: "Marriagetoxin", img: "anime/marriagetoxin.jpg", note: "épisode 2" },
